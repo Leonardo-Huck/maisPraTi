8f707cdc65a8a2658591ef4f191a70307d5bb7b5
Leonardo <leonardomonteirolima@gmail.com>
2024-05-03 21:31:05 -0300

    Realizado os exercicios do 4 ao 14

--- ListasdeExercicios/Lista01.js
+++ ListasdeExercicios/Lista01.js
@@ -11,7 +11,7 @@ const prompt = require('prompt-sync')();
 // 01 - 1. Escreva um algoritmo para ler uma temperatura em graus Celsius, calcular e escrever o
 // valor correspondente em graus Fahrenheit.
 
-// console.log('------------ CONVERSOR DE TEMPERATURA °C PARA °F ------------');
+// console.log('\n------------ CONVERSOR DE TEMPERATURA °C PARA °F ------------');
 // let grausCelsius = Number(prompt('Digite a temperatura em °C para converter : '));
 // let grausFahrenheit = (grausCelsius * 9 / 5) + 32;
 
@@ -24,7 +24,7 @@ const prompt = require('prompt-sync')();
 votos brancos, nulos e válidos. Calcular e escrever o percentual que cada um representa
 em relação ao total de eleitores. */
 
-// console.log('------------ CONTROLE DE VOTOS ------------');
+// console.log('\n------------ CONTROLE DE VOTOS ------------');
 // console.log('Insira as informações');
 
 // let totalDeEleitores = parseInt(prompt('Total de eleitores: '));
@@ -51,7 +51,7 @@ em relação ao total de eleitores. */
 ● Armazene no quarto inteiro a soma dos valores originais (os que o usuário digitou)
 dos primeiros três inteiros.*/
 
-// console.log('------------ INTEIROS ------------');
+// console.log('\n------------ INTEIROS ------------');
 
 // let primeiroInteiro = parseInt(prompt('Digite o primeiro inteiro: '));
 // let segundoInteiro = parseInt(prompt('Digite o segundo inteiro: '));
@@ -67,3 +67,260 @@ dos primeiros três inteiros.*/
 // console.log(segundoInteiro);
 // console.log(terceiroInteiro);
 // console.log(quartoInteiro);
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+/*4. Escreva um algoritmo para ler as notas das duas avaliações de um aluno no semestre,
+calcular e escrever a média semestral e a seguinte mensagem: ‘PARABÉNS! Você foi
+aprovado’, somente se o aluno foi aprovado (considere 6.0 a nota mínima para
+aprovação).*/
+
+// console.log('\n------------ MÉDIA SEMESTRAL ------------');
+// console.log();
+
+// let primeiraAvaliacao = Number(prompt('Informe a nota da primeira avaliação: '));
+// let segundaAvaliacao = Number(prompt('Informe a nota da segunda avaliação: '));
+
+// let mediaSemestre = (primeiraAvaliacao + segundaAvaliacao) / 2;
+
+// console.log(`A media do aluno foi ${mediaSemestre}!`);
+// if (mediaSemestre >= 6) {
+//     console.log('PARABÉNS! Você foi aprovado');
+// } else if (mediaSemestre < 6) {                           //Exercício 05
+//     console.log('Você foi REPROVADO! Estude mais');       //Exercício 05
+// }
+
+/*5. Acrescente ao exercício acima a mensagem ‘Você foi REPROVADO! Estude mais’ caso a
+média calculada seja menor que 6,0*/
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+/*6. Ler três valores para os lados de um triângulo: A, B e C. Verificar se os lados fornecidos
+formam realmente um triângulo. Caso forme, deve ser indicado o tipo de triângulo:
+Isósceles, escaleno ou eqüilátero.
+Para verificar se os lados fornecidos formam triângulo: A < B + C e B < A + C e C < A + B
+Triângulo isósceles: possui dois lados iguais (A=B ou A=C ou B = C)
+Triângulo escaleno: possui todos os lados diferentes (A<>B e B <> C)
+Triângulo eqüilátero: possui todos os lados iguais (A=B e B=C)*/
+
+// console.log('\n------------ TRIANGULO ------------');
+// console.log();
+
+// let ladoA = parseInt(prompt("Insira o comprimento do primeiro lado:"));
+// let ladoB = parseInt(prompt("Insira o comprimento do segundo lado:"));
+// let ladoC = parseInt(prompt("Insira o comprimento do terceiro lado:"));
+
+// if (ladoA + ladoB > ladoC && ladoA + ladoC > ladoB && ladoB + ladoC > ladoA) {
+//     console.log("É possível formar um triângulo com esses comprimentos!");
+//     if (ladoA === ladoB && ladoB === ladoC) {
+//         console.log('Este triangulo é eqüilátero pois possui todos os lados iguais');
+//     } else if (ladoA != ladoB && ladoB != ladoC && ladoA != ladoC) {
+//         console.log('Este triangulo é escaleno pois possui todos os lados diferentes');
+//     } else if (ladoA === ladoB || ladoA === ladoC || ladoB === ladoC) {
+//         console.log('Este triangulo é isósceles pois possui dois lados iguais');
+//     }
+// } else {
+//     console.log("Não é possível formar um triângulo com esses comprimentos!");
+// }
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+/*7. As maçãs custam R$ 0,30 se forem compradas menos do que uma dúzia, e R$ 0,25 se
+forem compradas pelo menos doze. Escreva um algoritmo que leia o número de maçãs
+compradas, calcule e escreva o valor total da compra.*/
+
+// console.log('\n------------ MAÇÃS ------------');
+// console.log();
+
+// let totalMacaes = parseInt(prompt('Informe o total de maçães compradas: '));
+// let totalDaCompra;
+
+// if (totalMacaes < 12 && totalMacaes > 0) {
+//     totalDaCompra = totalMacaes * 0.3;
+// } else if (totalMacaes >= 12) {
+//     totalDaCompra = totalMacaes * 0.25;
+// } else {
+//     totalDaCompra = -1
+// }
+
+// if (totalDaCompra === -1) {
+//     console.log('Informe uma quantidade válida');
+// } else {
+//     totalDaCompra = totalDaCompra.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+//     console.log(`O valor total da compra é de ${totalDaCompra}`);
+// }
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+/*8. Escreva um algoritmo para ler 2 valores (considere que não serão lidos valores iguais)
+e escreve-los em ordem crescente.*/
+
+// console.log('\n------------ 2 VALORES ------------');
+// console.log();
+
+// let primeiroValor = Number(prompt('Informe o primeiro valor: '));
+// let segundoValor = Number(prompt('Informe o segundo valor: '));
+
+// console.log('Os valores em ordem crescente são: ')
+
+// if (primeiroValor > segundoValor) {
+//     console.log(`{${segundoValor}, ${primeiroValor}}`);
+// } else {
+//     console.log(`{ ${primeiroValor}, ${segundoValor} }`);
+// }
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+/*9. Escreva um algoritmo que leia o código de origem de um produto e imprima a região
+do mesmo, conforme a tabela:
+
+1 - Sul             5 ou 6    - Nordeste
+2 - Norte           7, 8 ou 9 - Sudeste
+3 - Leste           10 até 20 - Centro-Oeste
+4 - Oeste           25 até 50 - Nordeste
+Fora dos intervalos - Produto Importado */
+
+// console.log('\n------------ CÓDIGO DE ORIGEM ------------');
+// console.log();
+
+// let codigoOrigem = parseInt(prompt('Informe o código de origem para verificação (1 ao 50) : '));
+
+// console.log('A origem do produto é: ');
+
+// if (codigoOrigem === 1) {
+//     console.log('1 - Sul');
+// } else if (codigoOrigem === 2) {
+//     console.log('2 - Norte');
+// } else if (codigoOrigem === 3) {
+//     console.log('3 - Leste');
+// } else if (codigoOrigem === 4) {
+//     console.log('4 - Oeste');
+// } else if (codigoOrigem === 5 || codigoOrigem === 6) {
+//     console.log('5 ou 6 - Nordeste');
+// } else if (codigoOrigem >= 7 && codigoOrigem <= 9) {
+//     console.log('7, 8 ou 9 - Sudeste');
+// } else if (codigoOrigem >= 10 && codigoOrigem <= 20) {
+//     console.log('10 até 20 - Centro-Oeste');
+// } else if (codigoOrigem >= 25 && codigoOrigem <= 50) {
+//     console.log('25 até 50 - Nordeste');
+// } else {
+//     console.log('Fora dos intervalos - Produto Importado');
+// }
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+//10. Escreva um algoritmo para ler um número inteiro e escrevê-lo na tela 10 vezes.
+
+// console.log('\n------------ REPETIÇÃO 10X ------------');
+// console.log();
+
+// let numero10X = parseInt(prompt('Informe um número para ser impresso 10 vezes: '));
+
+// for (i = 0; i < 10; i++) {
+//     console.log(numero10X);
+// }
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+/*11. Escreva um algoritmo para ler uma quantidade indeterminada de valores inteiros. Para
+cada valor fornecido escrever uma mensagem que indica se cada valor fornecido é PAR
+ou ÍMPAR. O algoritmo será encerrado imediatamente após a leitura de um valor NULO ou
+NEGATIVO.*/
+
+// console.log('\n------------ PAR OU ÍMPAR INFINITO ------------');
+// console.log();
+
+
+// let parOuImpar = 0;
+// do {
+//     parOuImpar = parseInt(prompt('Digite um número para verificar se é Par ou Ímpar (-1 ou nulo para sair): '));
+//     if (parOuImpar % 2 === 0) {
+//         console.log(`O número ${parOuImpar} é um número PAR!`)
+//     } else if (parOuImpar % 2 === 1) {
+//         console.log(`O número ${parOuImpar} é um número ÍMPAR!`)
+//     } else {
+//         console.log('Número Inválido!')
+//     }
+// } while (parOuImpar >= 0 && parOuImpar != null);
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+/* 12. Escreva um algoritmo que gere os números de 1000 a 1999 e escreva aqueles que,
+divididos por 11, dão resto igual a 5.*/
+
+// console.log('\n------------ NUMEROS DE 1000 a 1999 COM RESTO IGUAL A 5 QUANDO DIVIDIDOS POR 11 ------------');
+// console.log();
+
+// for (i = 1000; i <= 1999; i++) {
+//     if (i % 11 === 5) {
+//         console.log(i);
+//     }
+// }
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+/*13. Escrever um algoritmo que leia 5 valores para uma variável N e, para cada um deles,
+calcule e mostre a tabuada de 1 até N. Mostre a tabuada na forma:
+1 x N = N
+2 x N = 2N
+3 x N = 3N*/
+
+// console.log('\n------------ TABUADA ATÉ N ------------');
+
+// let n;
+// for (i = 0; i < 5; i++) {
+//     console.log();
+//     n = parseInt(prompt('Digite um valor para ver sua tabuada: '));
+//     for (operando = 1; operando <= n; operando++) {
+//         console.log(`${operando} X ${n} = ${operando * n}`);
+//     }
+// }
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+/*Utilizar somente while do exercício 14 ao 16
+
+14. Fazer um algoritmo para receber números decimais até que o usuário digite 0 e fazer
+a média aritmética desses números.*/
+
+console.log('\n------------ MÉDIA ARITMÉTICA ------------');
+console.log();
+
+let flagMedia = 1;
+let valorMedia;
+let totalMedia = 0;
+let somaMedia = 0;
+
+while (flagMedia !== 0) {
+    valorMedia = Number(prompt('Informe um decimal para calculo da média aritmética ( 0 para sair ): '));
+    if (valorMedia === 0) {
+        flagMedia = 0;
+    } else {
+        somaMedia += valorMedia;
+        totalMedia++;
+    }
+}
+
+if (somaMedia != 0) {
+    console.log(`A soma total dos decimais é de ${somaMedia}`);
+    console.log(`A média aritmética é de ${somaMedia / totalMedia}`);
+} else {
+    console.log('Saindo do programa...');
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+/*15. Fazer um algoritmo para receber um número decimal e o peso de cada número até
+que o usuário digite o número 0. Fazer a média ponderada desses números e pesos
+respectivos.*/
+
+// console.log('\n------------ MÉDIA PONDERADA ------------');
+// console.log();
