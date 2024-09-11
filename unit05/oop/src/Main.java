70a858cec4b6d8b60e6180a34b94969e35db612d
Leonardo-Huck <leonardomonteirohuck@gmail.com>
2024-09-10 23:26:47 -0300

    Streams e threads

--- unit05/oop/src/Main.java
+++ unit05/oop/src/Main.java
@@ -1,30 +1,21 @@
-import abstraction.ContaBancaria;
-import encapsulation.BankAccount;
-import encapsulation.BankTransaction;
+import singleton.ConfigurationManager;
+import polymorphism.Animal;
+import polymorphism.Dog;
+import polymorphism.Cat;
 
 public class Main {
     public static void main(String[] args) {
-        BankAccount account = new BankAccount("12345", 1000);
-
-        BankTransaction depositTransaction = new BankTransaction(account, 15000, BankTransaction.TransactionType.DEPOSIT);
-        depositTransaction.execute();
-
-        System.out.println(depositTransaction.getDetails());
-
-
-//        try {
-//            ContaBancaria contaJose = new ContaBancaria("1234", "Jose da Silva", 1000);
-//            ContaBancaria contaJoao = new ContaBancaria("5678", "João Pereira", 2000);
+//        ConfigurationManager configManager = ConfigurationManager.getInstance();
 //
-//            contaJose.depositar(1000);
-//            contaJose.sacar(800);
-//
-//            contaJose.transferir(100, contaJoao);
-//
-//            contaJose.exibirExtrato();
-//            contaJoao.exibirExtrato();
-//        }catch (Exception error){
-//            System.out.println(error.getMessage());
-//        }
+//        String dbUser = configManager.getProperty("db.user");
+//        System.out.println("Usuário do banco de dados " + dbUser);
+
+        Animal myAnimal = new Animal();
+        Dog myDog = new Dog();
+        Cat myCat = new Cat();
+
+        myAnimal.makeSound();
+        myDog.makeSound();
+        myCat.makeSound();
     }
 }
